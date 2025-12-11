6d3663da23fb04dc60c5db6bca09068f74df334a
Uday Patil <patiluday7122003@gmail.com>
2025-12-11 23:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 //your JS code here. If required.
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existingBtn = document.getElementById("existing");
+
+// Check if saved login exists in localStorage
+const savedUser = localStorage.getItem("username");
+const savedPass = localStorage.getItem("password");
+
+if (savedUser && savedPass) {
+  existingBtn.style.display = "block"; // Show existing user button
+}
+
+let submitbtn= document.getElementById("submit");
+submitbtn.addEventListener('submit' (e)=>{
+	e.preventDefault();
+
+	let username = usernameInput.value;
+	let password = passwordInput.value;
+
+	alert(`Logged in as ${username}`)
+	
+	if(checkbox.checked){
+		localStorage.setItem("username" , username);
+		localStorage.setItem("password" , password);
+		
+	}else{
+		localStorage.removeItem("username");
+		localStorage.removeItem("password");
+		
+	}
+
+	if(localStorage.getItem("username")){
+		existingBtn.style.display = "block";
+	}else{
+		existingBtn.style.display = "none";
+	}
+});
+
+
+existingBtn.addListener('submit' ,()=>{
+	let username = localStorage.getItem("username");
+	alert(`Logged in as saved ${username}`)
+})
+
+
+
+
+
+
